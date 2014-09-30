0305e510f432eca3e9813536674e5a8e315f5eb1
robertleeplummerjr <Adminuser1>
2014-09-29 22:13:15 -0400

    Tons of bugs fixed and performance enhancements Add levenshtein to aid with value lookups. Tab container css correction

--- Sheet/utilities.js
+++ Sheet/utilities.js
@@ -137,7 +137,56 @@ var arrHelpers = window.arrHelpers = {
         }
         //no length
         return false;
-    }
+    },
+	//http://stackoverflow.com/questions/11919065/sort-an-array-by-the-levenshtein-distance-with-best-performance-in-javascript
+	levenshtein: (function() {
+		var row2 = [];
+		return function(s1, s2) {
+			if (s1 === s2) {
+				return 0;
+			} else {
+				var s1_len = s1.length, s2_len = s2.length;
+				if (s1_len && s2_len) {
+					var i1 = 0, i2 = 0, a, b, c, c2, row = row2;
+					while (i1 < s1_len)
+						row[i1] = ++i1;
+					while (i2 < s2_len) {
+						c2 = s2.charCodeAt(i2);
+						a = i2;
+						++i2;
+						b = i2;
+						for (i1 = 0; i1 < s1_len; ++i1) {
+							c = a + (s1.charCodeAt(i1) === c2 ? 0 : 1);
+							a = row[i1];
+							b = b < a ? (b < c ? b + 1 : c) : (a < c ? a + 1 : c);
+							row[i1] = b;
+						}
+					}
+					return b;
+				} else {
+					return s1_len + s2_len;
+				}
+			}
+		};
+	})(),
+	lSearch: function(arr, value) {
+		var i = 0,
+			item,
+			max = arr.length,
+			found = -1,
+			distance;
+
+		for(;i < max; i++) {
+			item = arr[i];
+			distance = new Number(this.levenshtein(item, value));
+			distance.item = item;
+			if (distance < found) {
+				found = distance;
+			}
+		}
+
+		return (distance !== undefined ? distance.item : null);
+	}
 };
 
 var dates = {
