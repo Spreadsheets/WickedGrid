8d055d87c3d2fe0e399790359e094a3c1abae9b0
Robert Plummer <RobertLeePlummerJr@Gmail.com>
2014-10-30 15:12:31 -0400

    More cleanup, make sure Globalize can be used.  More removal of jQuery where possible.

--- Sheet/CellTypeHandlers.js
+++ Sheet/CellTypeHandlers.js
@@ -1,62 +1,65 @@
-Sheet.CellTypeHandlers = {
-	percent: function(cell, value) {
-		var num = (n(value) ? globalize.parseFloat(value) : value * 1),
-			result;
+Sheet.CellTypeHandlers = (function() {
+	var n = isNaN;
+	return {
+		percent: function (cell, value) {
+			var num = (n(value) ? Globalize.parseFloat(value) : value * 1),
+				result;
 
-		if (!n(num)) {//success
-			result = new Number(num);
-			result.html = globalize.format(num, 'p');
-			return result;
-		}
+			if (!n(num)) {//success
+				result = new Number(num);
+				result.html = Globalize.format(num, 'p');
+				return result;
+			}
 
-		return value;
-	},
-	date: function(cell, value) {
-		var date = globalize.parseDate(value);
-		if (date === null) {
 			return value;
-		} else {
-			date.html = globalize.format(date, 'd');
-			return date;
-		}
-	},
-	time: function(cell, value) {
-		var date = globalize.parseDate(value);
-		if (date === null) {
+		},
+		date: function (cell, value) {
+			var date = Globalize.parseDate(value);
+			if (date === null) {
+				return value;
+			} else {
+				date.html = Globalize.format(date, 'd');
+				return date;
+			}
+		},
+		time: function (cell, value) {
+			var date = Globalize.parseDate(value);
+			if (date === null) {
+				return value;
+			} else {
+				date.html = Globalize.format(date, 't');
+				return date;
+			}
+		},
+		currency: function (cell, value) {
+			var num = (n(value) ? Globalize.parseFloat(value) : value * 1),
+				result;
+
+			if (!n(num)) {//success
+				result = new Number(num);
+				result.html = Globalize.format(num, 'c');
+				return result;
+			}
+
 			return value;
-		} else {
-			date.html = globalize.format(date, 't');
-			return date;
-		}
-	},
-	currency: function(cell, value) {
-		var num = (n(value) ? globalize.parseFloat(value) : value * 1),
-			result;
+		},
+		number: function (cell, value) {
+			var radix, result;
+			if (!settings.endOfNumber) {
+				radix = Globalize.culture().numberFormat['.'];
+				settings.endOfNumber = new RegExp("([" + (radix == '.' ? "\." : radix) + "])([0-9]*?[1-9]+)?(0)*$");
+			}
 
-		if (!n(num)) {//success
-			result = new Number(num);
-			result.html = globalize.format(num, 'c');
-			return result;
-		}
+			if (!n(value)) {//success
+				result = new Number(value);
+				result.html = Globalize.format(value + '', "n10")
+					.replace(settings.endOfNumber, function (orig, radix, num) {
+						return (num ? radix : '') + (num || '');
+					});
+				return result;
+			}
 
-		return value;
-	},
-	number: function(cell, value) {
-		var radix, result;
-		if (!settings.endOfNumber) {
-			radix = globalize.culture().numberFormat['.'];
-			settings.endOfNumber = new RegExp("([" + (radix == '.' ? "\." : radix) + "])([0-9]*?[1-9]+)?(0)*$");
+			return value;
 		}
-
-		if (!n(value)) {//success
-			result = new Number(value);
-			result.html = globalize.format(value + '', "n10")
-				.replace(settings.endOfNumber, function (orig, radix, num) {
-					return (num ? radix : '') + (num || '');
-				});
-			return result;
-		}
-
-		return value;
-	}
-};
+	};
+})();
