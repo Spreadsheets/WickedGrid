e18ba6dbc789b74c8df4ae688dca55861f1514b7
robertleeplummerjr <Adminuser1>
2014-11-07 14:11:21 -0500

    Column unit tests

--- test/adding.cells.js
+++ test/adding.cells.js
@@ -49,3 +49,53 @@ original2'
 	tf.assert(test.assertEquals(div.find('table.jS td').length, 1 * 3), 'cell count is correct');
 	div.getSheet().kill();
 });
+
+tf.test('Behaviour Testing: Adding Columns', function(tf) {
+	//unit test
+	var div = $('<div>')
+			.append(tableify('original'))
+			.sheet(),
+		jS = div.getSheet(),
+		td;
+
+	jS.controlFactory.addColumn();
+
+	td = div.find('table.jS td:contains("original")');
+
+	tf.assert(test.assertEquals(td[0].cellIndex, 1), 'original @ expected location');
+	tf.assert(test.assertEquals(td[0].nextSibling.cellIndex, 2), 'added @ expected location');
+	tf.assert(test.assertEquals(div.find('table.jS td').length, 1 * 2), 'cell count is correct');
+	div.getSheet().kill();
+
+
+	//unit test
+	div = $('<div>')
+		.append(tableify('original'))
+		.sheet();
+	jS = div.getSheet();
+
+	jS.controlFactory.addColumn(1, true);
+
+	td = div.find('table.jS td:contains("original")');
+
+	tf.assert(test.assertEquals(td[0].cellIndex, 2), 'original @ expected location');
+	tf.assert(test.assertEquals(td[0].nextSibling, null), 'added @ expected location');
+	tf.assert(test.assertEquals(div.find('table.jS td').length, 1 * 2), 'cell count is correct');
+	div.getSheet().kill();
+
+
+	//unit test
+	div = $('<div>')
+		.append(tableify('original1\toriginal2'
+		))
+		.sheet();
+	jS = div.getSheet();
+
+	jS.controlFactory.addColumn(2, true);
+
+	td = div.find('table.jS td:contains("original1")');
+	tf.assert(test.assertEquals(td[0].cellIndex, 1), 'original @ expected location');
+	tf.assert(test.assertEquals(td.next().html(), ''), 'added @ expected location');
+	tf.assert(test.assertEquals(div.find('table.jS td').length, 1 * 3), 'cell count is correct');
+	div.getSheet().kill();
+});
