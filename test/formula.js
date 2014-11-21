20b87af90d89f344c34259900463ee91134b7353
Robert Plummer <RobertLeePlummerJr@Gmail.com>
2014-11-20 21:19:56 -0500

    dded another unit test to double check dependency behaviour.

--- test/formula.js
+++ test/formula.js
@@ -22,3 +22,52 @@ tf.test('Formula: Dependencies', function() {
 	tf.assert(test.assertEqual(B1.dependencies[0], C1), 'B1 is a dependency of C1');
 	div.getSheet().kill();
 });
+
+
+tf.test('Formula: Dependencies from JSON', function() {
+	var spreadsheets = [{
+			title: '1',
+			rows: [{
+				columns: [{},{formula:'A1 + 1'}]
+			}]
+		},{
+			title: '2',
+			rows: [{
+				columns: [{},{formula:'Sheet1!A1 + 100'}]
+			}]
+		},{
+			title: '3',
+			rows: [{
+				columns: [{},{formula:'Sheet1!A1 + 100'}]
+			}]
+		}],
+		loader = new Sheet.JSONLoader(spreadsheets),
+		div = $('<div>')
+			.sheet({
+				loader: loader,
+				minSize: {
+					rows: 1,
+					cols: 1
+				}
+			}),
+		jS = div.getSheet(),
+		sheet1A1 = jS.getCell(0, 1, 1), sheet1B1 = jS.getCell(0, 1, 2),
+		sheet2A1 = jS.getCell(1, 1, 1), sheet2B1 = jS.getCell(1, 1, 2),
+		sheet3A1 = jS.getCell(2, 1, 1), sheet3B1 = jS.getCell(2, 1, 2);
+
+	sheet1A1.updateValue();
+	sheet1B1.updateValue();
+	sheet2A1.updateValue();
+	sheet2B1.updateValue();
+	sheet3A1.updateValue();
+	sheet3B1.updateValue();
+	//thawing, so force values to resolve
+	sheet1A1.value = 100;
+	sheet1A1.setNeedsUpdated();
+	sheet1A1.updateValue();
+
+	tf.assertSame(sheet1B1.value.valueOf(), 101, 'A1 is a dependency of B1');
+	tf.assertSame(sheet2B1.value.valueOf(), 200, 'A2 is a dependency of B1');
+	tf.assertSame(sheet3B1.value.valueOf(), 200, 'B1 is a dependency of C1');
+	div.getSheet().kill();
+});
